efab37bddbe535f94ebd5805c9f95c2643eb729c
Khanh Nguyen Hoang <74997701+nhk2712@users.noreply.github.com>
2023-05-30 11:22:56 +0700

    Change lan_2 to lan_3

--- api/StudentModel.js
+++ api/StudentModel.js
@@ -4,7 +4,7 @@ async function StudentModel(studentId,dob){
     try{
         await Client.connect();
 
-        const StudentCollection = await Client.db("diem_khao_sat").collection("lan_2");
+        const StudentCollection = await Client.db("diem_khao_sat").collection("lan_3");
 
         var dobWithSlashes = dob.charAt(0)+dob.charAt(1)+"/"+dob.charAt(2)+dob.charAt(3)+"/";
         for (var i=4;i<8;i++) dobWithSlashes+=dob.charAt(i);
